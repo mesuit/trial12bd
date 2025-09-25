e5549795f7038547926ffc0096860b518ed77087
mesuit <makatech92@gmail.com>
2025-09-25 02:45:14 -0700

    Update urls.py

--- assignment_portal/urls.py
+++ assignment_portal/urls.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("dashboard.urls")),
+    path("api/", include("core.urls")),
 ]
-
-# serve media in dev
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
