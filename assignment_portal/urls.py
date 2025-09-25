ecbe5b4491c24d92fd430349c7df8a228c12ca4e
mesuit <makatech92@gmail.com>
2025-09-25 01:58:28 -0700

    Update urls.py

--- assignment_portal/urls.py
+++ assignment_portal/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("dashboard.urls")),   # 👈 matches your frontend API calls
+    path("api/", include("dashboard.urls")),
 ]
+
+# serve media in dev
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
